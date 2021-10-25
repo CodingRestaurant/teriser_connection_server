ea6bc1bda5facdc539885193abf17b10cbeaf07a
Kasania <nasang1997@gmail.com>
2021-10-26 04:30:02 +0900

    Add Main class

---
+++ src/main/java/com/codrest/TeriserConnectionServerApplication.java
@@ -0,0 +1,16 @@
+/*
+ * Author : Kasania
+ * Filename : TeriserConnectionServerApplication
+ * Desc :
+ */
+package com.codrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeriserConnectionServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TeriserConnectionServerApplication.class);
+    }
+}
