1b36e9f47575ae85e53bc8f8f276bf76f41b2859
Kasania <nasang1997@gmail.com>
2021-11-17 18:02:21 +0900

    Update build.grradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,3 +40,16 @@ val compileTestKotlin: KotlinCompile by tasks
 compileTestKotlin.kotlinOptions {
     jvmTarget = "1.8"
 }
+
+tasks.withType<Jar>() {
+
+    duplicatesStrategy = DuplicatesStrategy.INCLUDE
+
+    manifest {
+        attributes["Main-Class"] = "com.codrest.teriser.connectionserver.MainKt"
+    }
+
+    configurations["compileClasspath"].forEach { file: File ->
+        from(zipTree(file.absoluteFile))
+    }
+}
